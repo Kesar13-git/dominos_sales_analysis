4e5f10d9646942242c319fc29eba24c54fa54222
Kesar Deaulkar <kesardeaulkar430@gmail.com>
2026-05-03 12:53:09 +0530

    Update dominos_sales_query.sql

--- dominos_sales_query.sql
+++ dominos_sales_query.sql
@@ -1,48 +1,85 @@
-Select * from pizza_sales;
+USE pizza_db;
+
+SELECT * FROM dbo.pizza_sales;
+
+--Which pizza categories generate high sales but low profit, and should pricing or cost be adjusted? 
+
+SELECT TOP 5
+    pizza_category,
+    SUM(total_price) AS total_revenue,
+    SUM(total_price - (quantity * unit_price*0.6)) AS total_profit,
+    ROUND(
+        SUM(total_price - (quantity * unit_price*0.6)) * 100.0 
+        / SUM(total_price), 2
+    ) AS profit_margin_percentage
+FROM dbo.pizza_sales
+GROUP BY pizza_category
+ORDER BY profit_margin_percentage ASC;
+
  
- Select SUM(total_price) As Total_Revenue from pizza_sales;
+--Which pizza sizes contribute most to revenue vs profit, and are any sizes underpriced? 
 
- Select SUM(total_price) / Count(Distinct(order_id)) AS Avg_Order_value from pizza_sales;
+SELECT pizza_size , 
+    SUM(total_price) AS total_revenue,
+    SUM(total_price - (quantity * unit_price*0.6)) AS total_profit,
+    ROUND(
+        SUM(total_price - (quantity * unit_price*0.6)) * 100.0 
+        / SUM(total_price), 2
+    ) AS profit_margin_percentage,
+      ROUND(
+        SUM(total_price) * 1.0 / COUNT(DISTINCT order_id), 2
+    ) AS avg_order_value 
+FROM dbo.pizza_sales
+GROUP BY pizza_size
+ORDER BY profit_margin_percentage ASC;
 
- Select SUM(quantity) AS Total_pizza_sold from pizza_sales;
 
- Select Count(Distinct Order_id) as Total_orders from pizza_sales;
+-- Are top-selling pizzas also the most profitable, or are they reducing overall margins? 
 
-  Select cast(cast(SUM(quantity) AS Decimal(10,2)) / cast(Count(distinct order_id) As Decimal(10,2)) As Decimal(10,2)) AS Avg_pizza_per_order from pizza_sales;   //cast is used to convert int values into decimal(10,2) means we will bw having 10 decimal points value after point and 2 means we will consider 2 values//
+SELECT TOP 5
+    pizza_name,
+    SUM(total_price) AS total_revenue,
+    SUM(total_price - (quantity * unit_price * 0.6)) AS total_profit,
+    ROUND(
+        SUM(total_price - (quantity * unit_price * 0.6)) * 100.0 
+        / SUM(total_price), 2
+    ) AS profit_margin
+FROM dbo.pizza_sales
+GROUP BY pizza_name
+ORDER BY total_revenue DESC;
 
-  Select Datename(dw,order_date) as order_day, count(distinct order_id) As Total_orders  
-  from pizza_sales 
-  group by Datename(dw,order_date)
-  order by count(distinct order_id) ;  //dw is date of week //
 
-  Select Datename(month,order_date) as order_day, count(distinct order_id) As Total_orders 
-  from pizza_sales 
-  group by Datename(month,order_date)
-  order by Total_orders desc ;
+-- Why are certain days busier, and how can the business increase sales on low-performing days? 
 
-Select pizza_category, sum(total_price) AS total_sales , sum(total_price)*100 / (Select sum(total_price ) from pizza_sales where Month(Order_date) = 1 ) AS Pct
-from pizza_sales  
-where Month(Order_date) = 1   
-Group by pizza_category;
+SELECT 
+    DATENAME(WEEKDAY, order_date) as day_name,
+    DATEPART(WEEKDAY, order_date) as day_number,
+    COUNT(DISTINCT order_id) as total_orders,
+    SUM(total_price) as total_revenue,
+    ROUND(SUM(total_price)*1.0 / COUNT(DISTINCT order_id),2) as avg_order_value
+FROM dbo.pizza_sales
+GROUP BY DATENAME(WEEKDAY, order_date), DATEPART(WEEKDAY, order_date)
+ORDER BY day_number;
 
-Select pizza_size, CAST(sum(total_price) AS Decimal(10,2)) AS total_sales , CAST(sum(total_price)*100 / (Select sum(total_price ) from pizza_sales where DATEPART (quarter, order_date) = 1  ) AS Decimal (10,2)) AS Pct
-from pizza_sales 
-where DATEPART (quarter, order_date) = 1
-Group by pizza_size
-Order by PCT desc 
 
-SELECT TOP 5 pizza_name , cast(SUM(total_price) AS DECIMAL (10,2)) AS Total_Revenue from pizza_sales 
-GROUP BY pizza_name 
-Order by Total_Revenue desc  
+-- Which time of day has highest and lowest sales? 
+-- DATEPART(HOUR, order_time)
 
-SELECT TOP 5 pizza_name , cast(SUM(total_price) AS DECIMAL (10,2)) AS Total_Revenue from pizza_sales 
-GROUP BY pizza_name 
-Order by Total_Revenue;    // For Bottom 5 best sellers 
+SELECT TOP 5
+    DATEPART(HOUR, order_time) as hour_of_day,
+    COUNT(DISTINCT order_id) as total_orders,
+    SUM(total_price) as total_revenue,
+    ROUND(SUM(total_price)*1.0 / COUNT(DISTINCT order_id) , 2) as avg_order_value
+FROM dbo.pizza_sales
+GROUP BY DATEPART(HOUR, order_time)
+ORDER BY total_orders DESC;
 
-SELECT TOP 5 pizza_name , cast(quantity) AS DECIMAL (10,2)) AS Total_Quantity from pizza_sales 
-GROUP BY pizza_name 
-Order by Total_Quantity desc ;
 
-SELECT TOP 5 pizza_name , COUNT(DISTINCT order_id) AS Total_Orders from pizza_sales 
-GROUP BY pizza_name 
-Order by Total_Orders desc;  
+SELECT TOP 5
+    DATEPART(HOUR, order_time) as day_hour,
+    COUNT(DISTINCT order_id) as total_orders,
+    SUM(total_price) as total_revenue,
+    ROUND(SUM(total_price)*1.0 / COUNT(DISTINCT order_id) , 2) as avg_order_value
+FROM dbo.pizza_sales
+GROUP BY DATEPART(HOUR, order_time)
+ORDER BY total_orders ASC;
